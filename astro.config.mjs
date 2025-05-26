e80378e68a8609adfe5316d1423aabdce513c5e5
studyxpert <info.studyxpert@gmail.com>
2025-05-26 16:13:09 +0530

    Update astro.config.mjs
    changes done in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react(), sitemap()],
+  adapter: netlify()
 });
